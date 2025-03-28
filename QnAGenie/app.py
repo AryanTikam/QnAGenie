4f03e5140650c6e96e00ccb88a9717be580b2da1
Aryan Tikam <140395178+AryanTikam@users.noreply.github.com>
2025-03-28 23:34:41 +0530

    Fixed Vercel Deployment
    I think

--- QnAGenie/app.py
+++ QnAGenie/app.py
@@ -87,6 +87,6 @@ def clean_json_output(response_text):
         return response_text
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
